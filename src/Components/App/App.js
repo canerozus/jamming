032fa2b6438ff7ccd7ee3b0bc9eb160b23ac7ac4
deepforce08 <canerozus@hotmail.com>
2021-08-07 18:23:36 +0300

    pass playlist to tracklist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,14 +6,16 @@ import SearchResults from '../SearchResults/SearchResults'
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            SearchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}
-            ,   {name: 'name2', artist: 'artist2', album: 'album2', id: 2}
-            ,   {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
-        
-        ]
+            SearchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 }
+                , { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }
+                , { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
+            playListName: 'My Playlist',
+            playListTracks: [{ name: 'playListName1', artist: 'playListArtist1', album: 'playListAlbum1', id: 4 },
+            { name: 'playListName2', artist: 'playListArtist2', album: 'playListAlbum2', id: 5 },
+            { name: 'playListName3', artist: 'playListArtist3', album: 'playListAlbum3', id: 6 }]
         }
 
     }
@@ -22,10 +24,11 @@ class App extends React.Component {
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                     <SearchBar /> 
+                    <SearchBar />
                     <div className="App-playlist">
-                        <SearchResults SearchResults = {this.state.SearchResults}/>
-                        <Playlist />
+                        <SearchResults SearchResults={this.state.SearchResults} />
+                        <Playlist playListName={this.state.playListName} 
+                                playListTracks={this.state.playListTracks}/>
                     </div>
                 </div>
             </div>
