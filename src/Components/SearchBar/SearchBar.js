77c1bd76220b2aeb3252f254adc913dd04fc5ef5
deepforce08 <canerozus@hotmail.com>
2021-08-17 11:40:25 +0300

    product finished

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -10,12 +10,12 @@ export default class SearchBar extends Component {
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
     }
-    
+
     search() {
         this.props.onSearch(this.state.term)
     }
     handleTermChange(e) {
-        this.setState({term:e.target.value});
+        this.setState({ term: e.target.value });
     }
     render() {
         return (
